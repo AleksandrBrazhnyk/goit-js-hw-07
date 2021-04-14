3c9ddea27b6f529baa6124d8d51826133d33f53e
AleksandrBrazhnyk <sanchomus@ukr.net>
2021-04-15 00:28:47 +0300

    change task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,40 +3,31 @@
 //Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 //Для добавления стилей, используй CSS-классы valid и invalid.
 
+
+
 const validationInput = document.querySelector('#validation-input');
 
 const inputAttributeValue = parseInt(validationInput.getAttribute('data-length'));
 
 
- validationInput.addEventListener('blur', onInputBlur);
-
-
-function onInputBlur(event) {
+validationInput.addEventListener('change', onInputChange);
+validationInput.addEventListener('focus', onInputFocus);
+ 
+function onInputChange(event) {
    // console.log(event.currentTarget.value.length); 
-    if (event.target.value.length === inputAttributeValue  ) {
-        validationInput.classList.remove('invalid');
+    if (event.currentTarget.value.length === inputAttributeValue  ) {
         validationInput.classList.add('valid');
+        
         console.log('Вы успешно ввели 6 символов:)');
-    } else {   
-        validationInput.classList.remove('valid');
-        validationInput.classList.add('invalid');
-        console.log('Введите толко 6 символов');
- }  
-    
+
+    } else {   validationInput.classList.add('invalid');
+            
+    }  
+     
 };
-// function changeValidationColor(validClass, invalidClass) {
-//     validationInput.classList.add(validClass);
-//     validationInput.classList.remove(invalidClass);
 
-// }
-// #validation-input {
-//   border: 3px solid #bdbdbd;
-// }
-
-// #validation-input.valid {
-//   border-color: #4caf50;
-// }
-
-// #validation-input.invalid {
-//   border-color: #f44336;
-// }
+ function onInputFocus() {
+     validationInput.classList.remove('valid', 'invalid');
+       
+};
+    
--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,6 @@
 //Напиши скрипт, который реагирует на изменение значения input#font - size - control(событие input) и изменяет инлайн - стиль span#text обновляя свойство font - size.
 //В результате при перетаскивании ползунка будет меняться размер текста
 
-// const fontSizeControl = document.querySelector('#font-size-control');
-// console.log("fontSizeControl", fontSizeControl)
-
-// const textFontSize = document.querySelector('#text');
-
-
 const fontSizeControl = document.querySelector('#font-size-control');
 const textFontSize = document.querySelector('#text');  
 
@@ -18,10 +12,4 @@ fontSizeControl.addEventListener("input",
 
 
 
-
-// fontSizeControl.addEventListener('input', chengeTextFontSize);
-
-// function chengeTextFontSize(event) {
-//     textFontSize.style.fontSize = event.value;
-// };
 
